2fdc4288b287470d57babc71ac842e8b247e795d
oprichniki <alfredobyok@gmail.com>
2026-05-22 08:15:59 -0300

    config del zumo #define
    first commit

---
+++ config.h.ino
@@ -0,0 +1,42 @@
+//DEFINE ARCHIVO
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//PINES DE LOS DRIVERS
+#define pinMotorIzqPWM
+#define pinMotorIzqIn1
+#define pinMotorIzqIn2
+
+#define pinMotorDerPWM
+#define pinMotorDerIn1
+#define pinMotorDerIn2
+
+//PINES DE SENSORES DE DISTANCIA
+#define pinSDistIzq
+#define pinSDistCen
+#define pinSDistDer
+//PINES DE SENSORES DE LINEQA
+#define pinLinFrenteIzq
+#define pinLinFrenteDer
+//FALTA DEFINIR PINESSSSSSS
+//DEFINIR CONFIGURACION
+#define umbralLin 500
+#define velBusqueda 150
+#define velAtaque 255
+#define tiempoArranque  5000
+
+#endif
+//VARIABLES
+
+
+void setup() {
+
+
+
+}
+
+void loop() {
+
+
+
+}
